9f85d379ee1fe2b909fefd9e54180ee8dd988510
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2017-04-12 21:03:52 +0200

    [TASK] Converted tabs -> spaces

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,24 +1,24 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
-	'title' => 'Golf Courses',
-	'description' => 'Displayed which golf courses played',
-	'category' => 'plugin',
-	'author' => 'Tomas Norre Mikkelsen',
-	'author_email' => 'tomasnorre@gmail.com',
-	'state' => 'alpha',
-	'internal' => '',
-	'uploadfolder' => '1',
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '',
-	'constraints' => [
-		'depends' => [
-			'typo3' => '7.6.0-8.9.99',
-			'static_info_tables' => '',
-		],
-		'conflicts' => [
-		],
-		'suggests' => [
-		],
-	],
+    'title'            => 'Golf Courses',
+    'description'      => 'Displayed which golf courses played',
+    'category'         => 'plugin',
+    'author'           => 'Tomas Norre Mikkelsen',
+    'author_email'     => 'tomasnorre@gmail.com',
+    'state'            => 'alpha',
+    'internal'         => '',
+    'uploadfolder'     => '1',
+    'createDirs'       => '',
+    'clearCacheOnLoad' => 0,
+    'version'          => '',
+    'constraints'      => [
+        'depends'   => [
+            'typo3'              => '7.6.0-8.9.99',
+            'static_info_tables' => '',
+        ],
+        'conflicts' => [
+        ],
+        'suggests'  => [
+        ],
+    ],
 ];
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,28 +2,28 @@
 defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'TNM.' . $_EXTKEY,
-	'GolfCourses',
-	[
-		'List' => 'list',
-	],
-	// non-cacheable actions
-	[
-		'List' => 'list',
-	]
+    'TNM.' . $_EXTKEY,
+    'GolfCourses',
+    [
+        'List' => 'list',
+    ],
+    // non-cacheable actions
+    [
+        'List' => 'list',
+    ]
 );
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'TNM.' . $_EXTKEY,
-	'GolfRounds',
-	[
-		'Rounds' => 'list',
+    'TNM.' . $_EXTKEY,
+    'GolfRounds',
+    [
+        'Rounds' => 'list',
 
-	],
-	// non-cacheable actions
-	[
-		'Rounds' => 'list',
-	]
+    ],
+    // non-cacheable actions
+    [
+        'Rounds' => 'list',
+    ]
 );
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$_EXTKEY] = \TNM\GolfCourses\Hook\Tcemain::class;
